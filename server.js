c01ac65c65aeae3f54c37a1afc0cd40eedebaff3
walgab <gabriel.diem5@gmail.com>
2023-07-03 19:54:42 -0300

    Added support for subdomain of shortener

--- server.js
+++ server.js
@@ -3,8 +3,17 @@ const app = express();
 const port = process.env.port || 8080;
 
 app.get("/*", (req, res) => {
-  res.status(200)
-     .send("Hello");
+  const firstSubdomain = req.subdomains[0];
+
+  if (firstSubdomain === "short"){
+    res.status(200)
+    .send("Future URL Shortener");
+    
+  } else {
+    res.status(200)
+    .send("Hello, you are here where no subdomains are present");
+  }
+
 })
 
 app.listen(port, () => {
